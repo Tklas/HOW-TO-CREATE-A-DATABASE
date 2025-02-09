83bb365ef138339b4f58e869e32a63d9ecd2be99
Tosin Ayinde <vda7001320atosin@gmail.com>
2025-02-09 14:54:07 +0100

    Add files via upload

---
+++ HOW TO CREATE DB.sql
@@ -0,0 +1,29 @@
+--Let us create our first table
+
+CREATE TABLE Price_list (Serial_Number Int,
+						 Product_Names Varchar,
+						 Product_id Varchar,
+						 No_of_Sales Int
+
+
+);
+
+SELECT *
+FROM Price_List
+
+--Create a table that has 7 columns
+--tosin ayinde
+CREATE TABLE Attendance(Serial_number Int,
+						Student_name Varchar,
+						Matric_number Varchar,
+						No_of_attendance Int,
+						Tutor Varchar,
+						Captain Varchar,
+						Student_id Varchar
+					
+);
+
+SELECT *
+FROM Attendance
+
+
